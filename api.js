200dbd17effbf80b5caf9337c48edf5ea2ea7767
alxch <alexandr_ch@mail.ru>
2025-02-02 18:18:21 +0500

    API: refact.

--- api.js
+++ api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var cors = require('cors');
 // const stm = require('./stm');
 router.use(cors());
-router.use(function (req, res, next) {
+router.use(async (req, res, next) => {
   console.log('Req: body=',req.body, 'params=',req.params, 'query=', req.query);
+  await new Promise(resolve=>setTimeout(resolve,700));
   next();
 });
 
-const delay = (seconds=1) => new Promise(resolve=>setTimeout(resolve,1000*seconds));
 let item = {key:'',price:'',name:'',src:''};
 let paymentMethod = '';
 const paymentDetails = {
@@ -18,7 +18,6 @@ const paymentDetails = {
 
 router.post('/select-item', async (req, res) => {
   item = req.body;
-  await delay();
   res.send(JSON.stringify({
     item,
     status: 'done'
@@ -27,7 +26,6 @@ router.post('/select-item', async (req, res) => {
 
 router.post('/select-payment-method', async (req, res) => {
   paymentMethod = req.body.paymentMethod;
-  await delay();
   res.send(JSON.stringify({
     paymentMethod,
     status: 'done'
@@ -35,7 +33,6 @@ router.post('/select-payment-method', async (req, res) => {
 });
 
 router.get('/payment-details', async (req, res) => {
-  await delay();
   res.send(JSON.stringify({
     paymentDetails,
     status: 'processing'
